721abaaa521da6f109314c26bc5dd7be77988918
Monica <mep823@gmail.com>
2018-12-05 15:22:47 -0800

    first attempt

---
+++ src/components/RoomList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room )});
+    });
+  }
+
+  render() {
+    return (
+      <ul className='roomList'>
+        {
+          this.state.rooms.map( (room) =>
+            <li className="roomName">
+              {room.name}
+            </li>
+          )
+        }
+      </ul>
+    );
+  }
+}
+
+export default RoomList;
