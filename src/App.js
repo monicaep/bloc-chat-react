d384c2ea5bb6d2d5fe4f96bd2ad2e63af5294352
Monica <mep823@gmail.com>
2018-12-05 17:32:22 -0800

    List Chat Rooms: completed

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ var config = {
 class App extends Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
@@ -26,7 +27,7 @@ class App extends Component {
           <h1> Bloc Chat </h1>
         </header>
         <section className="roomList">
-          <RoomList />
+          <RoomList firebase={firebase} />
         </section>
       </div>
     );
