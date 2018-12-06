455ab66ab4ac88021813e88a450af47b130f9a71
Monica <mep823@gmail.com>
2018-12-06 15:38:23 -0800

    Create Chat Rooms: completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBADbW1igpUkg7rsxxPqqzVzRSG1wbQ76s",
+    authDomain: "bloc-chat-react-d9e6c.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-d9e6c.firebaseio.com",
+    projectId: "bloc-chat-react-d9e6c",
+    storageBucket: "bloc-chat-react-d9e6c.appspot.com",
+    messagingSenderId: "302018258901"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header>
           <h1> Bloc Chat </h1>
         </header>
+        <section className="roomList">
+          <RoomList
+            firebase={firebase}
+          />
+        </section>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: [],
+      newRoomName: ''
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room )});
+    });
+  }
+
+  handleChange(e) {
+    this.setState({newRoomName: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.newRoomName });
+    this.setState({ newRoomName: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul className='roomList'>
+          {
+            this.state.rooms.map( (room, index) =>
+              <li className='roomName' key={index}>
+                {room.name}
+              </li>
+            )
+          }
+        </ul>
+        <form className='createRoom' onSubmit={(e) => this.createRoom(e)} >
+          <input type="text" value={this.state.newRoomName} onChange={(e) => this.handleChange(e)} />
+          <input type="submit" value="Create Room" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RoomList;
